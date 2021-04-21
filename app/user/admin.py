013e49f0fc4c1a3b0dd9a47126dbc61d451c05cd
Soren <swacker@ucalgary.ca>
2021-04-21 21:29:18 +0000

    ..

--- app/user/admin.py
+++ app/user/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    inlines = (ProfileInline, )
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff')
+    list_select_related = ('profile', )
+
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
