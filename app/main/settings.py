ff6f5dbdc9f719428797c32213980228630fd4df
Soren <swacker@ucalgary.ca>
2021-04-21 19:27:17 +0000

    ...

--- app/main/settings.py
+++ app/main/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = P(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
-HOME_TITLE = os.getenv('HOME_TITLE', 'Omics Pipelines')
+HOME_TITLE = os.getenv('HOME_TITLE', 'Django-3 Omics Pipelines')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.getenv('ENVIRONMENT') == 'develop')
