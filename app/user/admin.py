0d2d33b11ea31d67a5cce53c27687360b006f53f
Soren <swacker@ucalgary.ca>
2021-04-21 21:32:08 +0000

    ..

--- app/user/admin.py
+++ app/user/admin.py
@@ -6,5 +6,4 @@ class CustomUserAdmin(UserAdmin):
     list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff')
     list_select_related = ('profile', )
 
-admin.site.unregister(User)
 admin.site.register(User, CustomUserAdmin)
