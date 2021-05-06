1f1e313ccf44942f5ef70ed80cbb1af6d426a8e7
Soren <swacker@ucalgary.ca>
2021-05-06 21:01:27 +0000

    Dashboard color by flagged/downstream use

--- app/dashboards/dashboards/dashboard/index.py
+++ app/dashboards/dashboards/dashboard/index.py
@@ -337,7 +337,7 @@ def plot_qc_figure(refresh, selected, ndxs, x, data, optional_columns):
     )    
     
 
-    marker_color = df['Use Downstream'].replace({True: 'rgb(158,202,225)', False: 'rgb(240,200,240)'})
+    marker_color = df['Use Downstream'].replace({True: 'rgb(158,202,225)', False: 'rgb(250,230,230)'})
     marker_line_color = df['Flagged'].replace({False: 'rgb(158,202,225)', True: 'rgb(200,0,0)'})
 
     print(marker_color)
