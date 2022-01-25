f3646334a59f68fa9ad5e53bb106443274f38fab
Soren <swacker@ucalgary.ca>
2022-01-25 00:50:52 +0000

    isolation forest fraction set to 0.05

--- app/dashboards/dashboards/dashboard/anomaly.py
+++ app/dashboards/dashboards/dashboard/anomaly.py
@@ -62,7 +62,7 @@ def callbacks(app):
         qc_data = pqc.get_qc_data(data_range=None).set_index("RawFile")
 
         predictions, df_shap = T.detect_anomalies(
-            qc_data, fraction=0.49, n_estimators=1000
+            qc_data, fraction=0.05, n_estimators=1000
         )
 
         # Update flags
