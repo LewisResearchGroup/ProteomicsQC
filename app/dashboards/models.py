e64038812c5d967668d8db33fc195d5016573867
Soren Wacker <swacker@ucalgary.ca>
2022-04-07 17:49:02 +0000

    Tidy up frontend

--- app/dashboards/models.py
+++ app/dashboards/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from .dashboards.proteomics_dashboard import app as proteomics_dashboard
+from .dashboards.dashboard.index import app as dashboard
 
 
 class StatelessProteomicsDashboardApp(models.Model):
@@ -13,44 +13,4 @@ class StatelessProteomicsDashboardApp(models.Model):
     slug = models.SlugField(max_length=110, unique=True, blank=True)
 
     def as_dash_app(self):
-        return proteomics_dashboard()
-
-
-class DashApp(models.Model):
-    """
-    An instance of this model represents a Dash application and its internal state
-    """
-
-    stateless_app = models.ForeignKey(
-        StatelessProteomicsDashboardApp,
-        on_delete=models.PROTECT,
-        unique=False,
-        null=False,
-        blank=False,
-    )
-    instance_name = models.CharField(
-        max_length=100, unique=True, blank=True, null=False
-    )
-    slug = models.SlugField(max_length=110, unique=True, blank=True)
-    base_state = models.TextField(null=False, default="{}")
-    creation = models.DateTimeField(auto_now_add=True)
-    update = models.DateTimeField(auto_now=True)
-    save_on_change = models.BooleanField(null=False, default=False)
-
-    def current_state(self):
-        """
-        Return the current internal state of the model instance
-        """
-        pass
-
-    def update_current_state(self, wid, key, value):
-        """
-        Update the current internal state, ignorning non-tracked objects
-        """
-        pass
-
-    def populate_values(self):
-        """
-        Add values from the underlying dash layout configuration
-        """
-        pass
+        return dashboard()
--- app/dashboards/urls.py
+++ app/dashboards/urls.py
@@ -2,5 +2,5 @@ from django.urls import path, include
 from .views import dashboard
 
 urlpatterns = [
-    path("", dashboard, name="primary_dashboard"),
+    path("", dashboard, name="dashboard"),
 ]
