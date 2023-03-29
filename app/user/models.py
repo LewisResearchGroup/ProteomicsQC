ac7dee0329de8ed05054bff811105a90996246cf
Soren Wacker <swacker@ucalgary.ca>
2023-03-29 01:23:29 +0000

    updae python

--- app/user/models.py
+++ app/user/models.py
@@ -2,7 +2,7 @@ from uuid import uuid4
 
 from django.db import models
 
-from django.contrib.auth.models import AbstractUser, AbstractBaseUser
+from django.contrib.auth.models import AbstractUser
 from django.utils.translation import ugettext_lazy as _
 
 from .managers import CustomUserManager
