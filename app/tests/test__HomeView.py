a12f56847bca19708da397e60308840725a6f00a
Mario E Valdes-Tresanco <marioe911116@gmail.com>
2026-02-21 21:56:21 -0700

    test: add unit test for quick steps using last activity for project and pipeline

--- app/tests/test__HomeView.py
+++ app/tests/test__HomeView.py
@@ -1,6 +1,9 @@
+from datetime import timedelta
+
 from django.test import TestCase
 from django.urls import reverse
 from django.core.files.uploadedfile import SimpleUploadedFile
+from django.utils import timezone
 
 from user.models import User
 from project.models import Project
@@ -52,3 +55,48 @@ class HomeViewTestCase(TestCase):
         quick_steps = response.context["quick_steps"]
         self.assertEqual(len(quick_steps), 4)
         self.assertEqual(quick_steps[0]["title"], "Open projects")
+
+    def test_quick_steps_use_last_activity_for_project_and_pipeline(self):
+        self.client.force_login(self.user)
+
+        project_recent_created = Project.objects.create(
+            name="Project 2", description="Second project", created_by=self.user
+        )
+        old_project = self.project
+
+        contents_mqpar = b"<mqpar></mqpar>"
+        contents_fasta = b">protein\nSEQUENCE"
+        old_pipeline = self.pipeline
+        new_pipeline = Pipeline.objects.create(
+            name="pipe2",
+            project=project_recent_created,
+            created_by=self.user,
+            fasta_file=SimpleUploadedFile("my_fasta_2.fasta", contents_fasta),
+            mqpar_file=SimpleUploadedFile("my_mqpar_2.xml", contents_mqpar),
+            rawtools_args="-p -q -x",
+        )
+
+        RawFile.objects.create(
+            pipeline=new_pipeline,
+            orig_file=SimpleUploadedFile("newer_created.raw", b"..."),
+            created_by=self.user,
+        )
+
+        old_project.created = timezone.now() - timedelta(days=10)
+        old_project.save(update_fields=["created"])
+        old_pipeline.created = old_project.created.date()
+        old_pipeline.save(update_fields=["created"])
+
+        old_result = old_pipeline.rawfile_set.first().result
+        old_result.created = timezone.now() + timedelta(minutes=5)
+        old_result.save(update_fields=["created"])
+
+        url = reverse("home")
+        response = self.client.get(url)
+        quick_steps = response.context["quick_steps"]
+
+        self.assertEqual(quick_steps[1]["note"], f"Continue in {old_project.name}.")
+        self.assertEqual(
+            quick_steps[2]["note"],
+            f"Open {old_pipeline.project.name} / {old_pipeline.name} upload queue.",
+        )
