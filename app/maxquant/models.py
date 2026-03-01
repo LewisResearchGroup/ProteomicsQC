15a18483908dd725f04ba271476b44916fc826e6
Sören Wacker <s.wacker@tudelft.nl>
2026-03-01 10:35:19 +0100

    Fix models.py exports and update deps for ARM compatibility

---
+++ app/maxquant/models.py
@@ -0,0 +1,19 @@
+from .RawFile import RawFile
+from .MaxQuantParameter import MaxQuantParameter
+from .MaxQuantExecutable import MaxQuantExecutable
+from .Pipeline import Pipeline
+from .FastaFile import FastaFile
+from .Result import Result
+from .rawtools import RawToolsSetup
+from .BasicUpload import BasicUpload
+
+__all__ = [
+    "RawFile",
+    "MaxQuantParameter",
+    "MaxQuantExecutable",
+    "Pipeline",
+    "FastaFile",
+    "Result",
+    "RawToolsSetup",
+    "BasicUpload",
+]
